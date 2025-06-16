48a79eccc03bc728ac777e662bc409904c8cbb20
phyo-thi-ha <phyothiha.900@gmail.com>
2025-06-16 16:05:33 +0630

    third commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, This is free python hosting by Phyo Thiha!'
 
 if __name__ == '__main__':
     app.run()
